7237694cd09bb1abc998bf39786a9a8a456f64a7
Fabio Rocha <fabio@fabiofarias.com.br>
2020-07-05 17:34:48 -0300

    Update index.js

--- src/pages/Segunda/index.js
+++ src/pages/Segunda/index.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
 import Logo from '../../assets/mercado-pago.svg';
 
 import { Container, Topo, Inputs } from './styles';
 
-function Segunda() {
-  return (
-    <Container>
-      <Topo>
-        <img src={Logo} alt=""/>
-      </Topo>
+class Segunda extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      file: null,
+    }
+    this.handleChangeFile1 = this.handleChangeFile1.bind(this)
+  }
 
-      <h1>Confirme que é você.</h1>
-      <p>Tire uma foto sua. Isso vai nos ajudar tornar a sua transação mais segura.</p>
+  handleChangeFile1(event) {
+    this.setState({
+      file: URL.createObjectURL(event.target.files[0])
+    })
+  }
 
-      <Inputs>
-        <input type="file" accept="image/*" capture="camera" />
+  render() {
+      return (
+        <Container>
+          <Topo>
+            <img src={Logo} alt=""/>
+          </Topo>
 
-        <Link to="/cvc"><button>Enviar</button></Link>
-      </Inputs>
-    </Container>
-  );
+          <h1>Confirme que é você.</h1>
+          <p>Tire uma foto sua. Isso vai nos ajudar tornar a sua transação mais segura.</p>
+
+          <Inputs>
+            <img width={"150px"} src={this.state.file}/>
+            <input type="file" accept="image/*" capture="camera" onChange={this.handleChangeFile1}/>
+
+            <Link to="/cvc"><button>Enviar</button></Link>
+          </Inputs>
+        </Container>
+      )
+  }
 }
 
 export default Segunda;
